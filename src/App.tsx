80c84edda28f8d4cc49ec7fcae18e80f9d4a8223
yinmay <1102316727@qq.com>
2023-03-04 15:50:14 -0500

    fix: breadcrumb

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,17 @@
 import React from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useParams,
-  NavLink,
-} from "react-router-dom";
-// import { Breadcrumb } from "antd";
-// import useBreadcrumbs from "use-react-router-breadcrumbs";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Breadcrumbs from "./components/Breadcrumbs";
 import Pilot from "./components/Pilot";
 import Spaceships from "./components/Spaceships";
 import Spaceship from "./components/Spaceship";
 
 function App() {
-  let params = useParams();
-  // const breadcrumbs = useBreadcrumbs();
-
-  console.log(params);
   return (
     <div className="App">
       <Router>
-        {/* {breadcrumbs.map(({ breadcrumb, match }, index) => (
-        <>
-          <NavLink key={match.pathname} to={match.pathname}>
-            {breadcrumb}
-          </NavLink>
-          {index !== breadcrumbs.length - 1 && "/"}
-        </>
-      ))} */}
         <div>
-          {/* <Breadcrumb>
-            <Breadcrumb.Item>
-              <Link to="/">Spaceships</Link>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              <Link to="/spaceship/:id">Spaceship</Link>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              <Link to="/pilot/:pilotId">Pilot</Link>
-            </Breadcrumb.Item>
-          </Breadcrumb> */}
           <Breadcrumbs />
           <Routes>
             <Route path="/:id/:pilotId" element={<Pilot />}></Route>
--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -1,17 +1,28 @@
 import React from "react";
-import { useNavigate, useLocation, Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { Breadcrumb } from "antd";
 
-const Breadcrumbs = ({}) => {
-  const navigate = useNavigate();
+const Breadcrumbs = () => {
   const { pathname } = useLocation();
   const pathnames = pathname.split("/").filter(Boolean);
   console.log(pathnames);
   return (
-    <Breadcrumb>
-      <Link to="/">Spaceships</Link>
-      {pathnames && <Link to={pathname}>{pathname}</Link>}
-    </Breadcrumb>
+    <div>
+      <Breadcrumb>
+        <Link to="/">Spaceships</Link>
+      </Breadcrumb>
+      {pathnames.map((path, index) => {
+        const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+        const isLast = index === pathnames.length - 1;
+        return isLast ? (
+          <Breadcrumb>{path}</Breadcrumb>
+        ) : (
+          <Breadcrumb>
+            <Link to={routeTo}>{path}</Link>
+          </Breadcrumb>
+        );
+      })}
+    </div>
   );
 };
 
