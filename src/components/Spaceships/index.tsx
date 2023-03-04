44ac0b685637d6c0c80b0fafd71bd0190b4b92e2
yinmay <1102316727@qq.com>
2023-03-04 13:42:12 -0500

    feat: add api of spaceships

--- src/components/Spaceships/index.tsx
+++ src/components/Spaceships/index.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+interface Spaceship {
+  name: string;
+  model: string;
+  manufacturer: string;
+  pilots: string[];
+}
 
 export default function Spaceships() {
+  const [spaceships, setSpaceships] = useState<Spaceship[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/starships")
+      .then((response) => {
+        console.log(response.data.results);
+        setSpaceships(response.data.results);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
   return <div>Spaceships</div>;
 }
